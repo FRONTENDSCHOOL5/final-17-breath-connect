a4e558391eaffef03b8fbd9c2e7d8fa31224fab8
sypaik.dev <sypaik.dev@gmail.com>
2023-06-15 02:32:55 +0900

    🎨 Style: GlobalStyle, Theme 컬러 속성 값 추가 (#28)

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/GlobalStyle';
 import AppRouter from './routes/AppRouter';
-import Theme from './styles/Theme'
-function App() {
+import Theme from './styles/Theme';
+
+const App = () => {
   return (
     <>
-    <ThemeProvider theme={Theme}>
-      <GlobalStyle />
-      <AppRouter />
-    </ThemeProvider>
+      <ThemeProvider theme={Theme}>
+        <GlobalStyle />
+        <AppRouter />
+      </ThemeProvider>
     </>
   );
-}
+};
+
 export default App;
---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function LoginPage() {
+  return (
+    <h1>내가 구현할 페이지</h1>
+  );
+}
+
+export default LoginPage;
