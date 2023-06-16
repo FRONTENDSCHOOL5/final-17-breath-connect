ae2f419987dfcf7f7c95feeb71cc6d135e5d93e7
sypaik.dev <sypaik.dev@gmail.com>
2023-06-16 23:38:52 +0900

    🔨 Build: recoil 설치 및 세팅

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 // import ReactDOM from 'react-dom'; //구버전
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { RecoilRoot } from 'recoil';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <RecoilRoot>
+    <App />
+  </RecoilRoot>
+);
