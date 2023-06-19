911abd279abcabd4db6f3576037143e64ce94102
yeonju.park <yeonjux@naver.com>
2023-06-19 14:27:45 +0900

    🎨 Style: 공통 컴포넌트 User 수정 (#80)

--- src/components/common/User/Follow/Follow.jsx
+++ src/components/common/User/Follow/Follow.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
-import PROFILEIMAGE  from '../../../../assets/Img/basic-profile-img.png';
-//import Button from '../../Button/ButtonContainer';
+import profileImage  from '../../../../assets/images/basic-profile-m.svg';
+import ButtonContainer from '../../Button/ButtonContainer';
 
 const Follow = () => {
   return (
     <UserItem>
-      <ProfileImage src={PROFILEIMAGE} alt='weniv_Mandarin님의 프로필 사진' width='50' />
+      <ProfileImage src={profileImage} alt='유저님의 프로필 사진' width='50' />
       <Wrapper>
         <UserName>애월읍 위니브 감귤농장</UserName>
         <UserId>weniv_Madarin</UserId>
       </Wrapper>
-      <button>팔로우</button>
+      <ButtonWrapper>
+      <ButtonContainer type={'S'} text={'팔로우'}></ButtonContainer>
+      </ButtonWrapper>
+
      </UserItem>
   );
 };
@@ -26,7 +29,7 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  margin-left: 12px;
+  margin-left: 1.2rem;
 `;
 
 const UserName = styled.strong`
@@ -46,7 +49,11 @@ const UserId = styled.strong`
 `;
 
 const ProfileImage = styled.img`
-  width: 50px;
+  width: 5rem;
   height: auto;
   border-radius: 50%;
 `;
+
+const ButtonWrapper = styled.button`
+  margin-top: 1rem;
+`
