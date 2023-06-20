d8f411cea9bcae1f7d7ba19b33add07af913bec8
holaleche <whoisleche@gmail.com>
2023-06-20 11:45:26 +0900

    :sparkles: Feat: 공통 컴포넌트 Modal UI 구현 (#93)

---
+++ src/components/common/Modal/PostModal.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Modal = ({ text }) => {
+
+  return (
+    <ModalWrapper>
+      <ModalContent>
+        <Separator />
+        <ModalButtons>
+          <button>{text}</button>
+        </ModalButtons>
+      </ModalContent>
+    </ModalWrapper>
+  );
+};
+
+export default Modal;
+
+const ModalContent = styled.div`
+  width: 39rem;
+  height: 9.2rem;
+  background-color: ${({ theme }) => theme.colors.whiteText};
+  border-top-left-radius: 0.8rem;
+  border-top-right-radius: 0.8rem;
+`;
+
+const Separator = styled.div`
+  width: 5rem;
+  height: 0.4rem;
+  background: ${({ theme }) => theme.colors.borderColor};
+  margin: 1.6rem 17rem;
+  border-radius: 0.5rem;
+`;
+
+const ModalButtons = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 2rem;
+  button{
+    text-align: left;
+    font-size: ${({ theme }) => theme.fontSize.medium};
+    padding: 1.3rem 0rem 1.4rem 2.6rem;
+  }
+`;
+
+const ModalWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 999;
+  opacity: 1;
+  pointer-events: none;
+`;
