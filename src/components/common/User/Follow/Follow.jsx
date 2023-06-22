0d0687c9e582ddbd74826bc06f283bf4197d1d7b
yeonju.park <yeonjux@naver.com>
2023-06-23 00:05:52 +0900

    ✨ Feat: 팔로워/팔로잉페이지 api연결 (#101)

--- src/components/common/User/Follow/Follow.jsx
+++ src/components/common/User/Follow/Follow.jsx
@@ -1,28 +1,33 @@
-import React, {useState} from 'react';
+import React, { useState  } from 'react';
 import styled from 'styled-components';
 import ButtonContainer from '../../Button/ButtonContainer';
 
-const Follow = ({ user }) => {
-  const { username, userId, profileImage } = user;
-  const [isFollowing, setIsFollowing] = useState(true);
+  const Follow = ({ user }) => {
+    const {
+      username,
+      intro,
+      image,
+      isfollow,
+    } = user;
+  
+    const [isFollowing, setIsFollowing] = useState(isfollow);
+    const handleClick = () => {
+      setIsFollowing(!isFollowing);
+    };
 
-  const handleClick = () => {
-    setIsFollowing(!isFollowing);
-  };
   return (
     <UserItem>
-      <ProfileImage src={profileImage} alt='유저님의 프로필 사진' width='50' />
+      <ProfileImage src={image} alt="유저님의 프로필 사진" width="50" />
       <Wrapper>
         <UserName>{username}</UserName>
-        <UserId>{userId}</UserId>
+        <Intro>{intro}</Intro>
       </Wrapper>
       <ButtonWrapper>
         <ButtonContainer
           type={'S'}
           text={isFollowing ? '팔로우' : '취소'}
           isClicked={!isFollowing}
-          handleClick={handleClick}
-        ></ButtonContainer>
+          handleClick={handleClick} />
       </ButtonWrapper>
     </UserItem>
   );
@@ -49,20 +54,18 @@ const UserName = styled.strong`
   font-weight: 500;
 `;
 
-const UserId = styled.strong`
+const Intro = styled.span`
   display: block;
+  margin-bottom: 1rem;
   font-size: ${({ theme }) => theme.fontSize.small};
   color: ${({ theme }) => theme.colors.textColor};
-  ::before {
-    content: '@ ';
-  }
 `;
 
 const ProfileImage = styled.img`
   width: 5rem;
   height: 5rem;
   border-radius: 50%;
-  border: 1px solid #767676;
+  //border: 1px solid #767676;
 `;
 
 const ButtonWrapper = styled.button`
---
+++ src/pages/FollowListPage/FollowingListPage.jsx
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import { tokenAtom } from '../../atoms/UserAtom';
+import { useRecoilValue } from 'recoil';
+import { useParams } from 'react-router-dom';
+import { getFollowingList } from '../../utils/Apis';
+import Follow from '../../components/common/User/Follow/Follow';
+import TopListNavHeader from '../../components/Header/TopListNavHeader';
+import TabMenu from '../../components/Footer/TabMenu';
+
+const FollowListPage = () => {  
+  const token = useRecoilValue(tokenAtom);
+  const account = useParams().id;
+  console.log(account);
+  const [followings, setFollowings] = useState([]);
+    useEffect(() => {
+    const followList = async () => {
+      // 팔로잉 리스트 목록
+      const data = await getFollowingList(account);
+      console.log(data); 
+      setFollowings(data);
+    };
+    followList();
+  }, [account, token]);
+
+  return (
+    <>
+      <TopListNavHeader />
+      <ContentsWrapper>
+        <FollowList>
+          {followings.length > 0 ? (
+            followings.map((user) => (
+              <Follow
+                key={user._id}
+                user={{
+                  _id: user._id,
+                  username: user.username,
+                  intro: user.intro,
+                  image: user.image,
+                  isfollow: user.isfollow,
+                }}
+              />
+            ))
+          ) : (
+            <NoFollowersText>팔로우한 사용자가 없습니다.</NoFollowersText>
+          )}
+        </FollowList>
+      </ContentsWrapper>
+      <TabMenu />
+    </>
+  );
+};
+
+export default FollowListPage;
+
+const ContentsWrapper = styled.main`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 27.5rem;
+  padding: 2.4rem 1.6rem;
+`;
+
+const FollowList = styled.ul`
+  li {
+    margin-bottom: 0.8rem;
+    height: 5rem;
+  }
+`;
+
+const NoFollowersText = styled.p`
+  margin-top: 2rem;
+  text-align: center;
+  font-size: 1.2rem;
+  color: #767676;
+`;
