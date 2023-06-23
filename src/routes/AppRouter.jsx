ad9a0280010e76588437929b7f2024903c153320
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2023-06-23 09:41:47 +0900

    Add: 팔로잉 리스트, 피드 디테일 페이지 라우터 추가

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -5,7 +5,10 @@ import LoginPage from '../pages/LoginPage/LoginPage';
 import SignupPage from '../pages/SignupPage/SignupPage';
 import ProfileSettingPage from '../pages/ProfileSettingPage/ProfileSettingPage';
 import FeedPage from '../pages/FeedPage/FeedPage';
+import FeedDetail from '../pages/FeedPage/FeedDetail';
 import ProfilePage from '../pages/ProfilePage/ProfilePage';
+import FollowerListPage from '../pages/FollowListPage/FollowerListPage';
+import FollowingListPage from '../pages/FollowListPage/FollowingListPage';
 import PostPage from '../pages/PostPage/PostPage';
 import UploadPage from '../pages/UploadPage/UploadPage';
 import UploadMap from '../pages/UploadPage/UploadMap';
@@ -13,7 +16,6 @@ import SearchPage from '../pages/SearchPage/SearchPage';
 import ChatListPage from '../pages/ChatPage/ChatListPage';
 import ChatRoomPage from '../pages/ChatPage/ChatRoomPage';
 import NotFoundPage from '../pages/NotFoundPage/NotFoundPage';
-import FollowListPage from '../pages/FollowListPage/FollowListPage';
 
 const AppRouter = () => {
   return (
@@ -24,13 +26,16 @@ const AppRouter = () => {
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/signup/profile" element={<ProfileSettingPage />} />
         <Route path="/home" element={<FeedPage />} />
+        <Route path="/home/feedetail/:account" element={<FeedDetail />} />
         <Route path="/profile/:account" element={<ProfilePage />} />
+         <Route path="/profile/:id" element={<ProfilePage />} />
+        <Route path="/profile/:id/follower" element={<FollowerListPage />} />
+        <Route path="/profile/:id/follow" element={<FollowingListPage />} />
         <Route path="/post" element={<PostPage />} />
         <Route path="/post/upload" element={<UploadPage />} />
         <Route path="/post/upload/map" element={<UploadMap />} />
         <Route path="/search" element={<SearchPage />} />
         <Route path="/chat" element={<ChatListPage />} />
-        <Route path="/follow" element={<FollowListPage />} />
         <Route path="/chat/:id" element={<ChatRoomPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
