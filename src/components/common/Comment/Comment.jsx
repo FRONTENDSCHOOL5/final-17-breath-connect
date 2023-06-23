251b3e16e6a467cceb44ec865d2ea0ff03d0ee92
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2023-06-24 02:13:47 +0900

    🎨 Style: 댓글 컴포넌트 UI 수정 및 버튼 활성화 추가

--- src/components/common/Comment/Comment.jsx
+++ src/components/common/Comment/Comment.jsx
@@ -1,29 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import profile from '../../../assets/images/profile.png';
+import { useParams } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { tokenAtom } from '../../../atoms/UserAtom';
+import BasicProfileImg from '../../../assets/images/basic-profile-xs.svg';
+import { postComment } from '../../../utils/Apis';
 
-const Comment = () => {
+const Comment = ({ data }) => {
+  const postId = useParams().id;
+  const userToken = useRecoilValue(tokenAtom);
+  const [inputComment, setInputComment] = useState('');
+
+  const handleInput = (e) => {
+    const comment = e.target.value;
+    setInputComment(comment);
+  };
+
+  const handleCommentSubmit = async (e) => {
+    e.preventDefault();
+    const response = await postComment(postId, inputComment);
+    console.log(response);
+  };
+
   return (
     <>
-      <CommentWrap>
+      <CommentContainer onSubmit={handleCommentSubmit}>
         <StyledComment>
-          <img src={profile} alt="프로필 비활성화" />
-          <CommentInput placeholder="댓글을 입력하세요..."></CommentInput>
+          <img src={BasicProfileImg} alt="프로필 비활성화" />
+          <CommentInput
+            placeholder="댓글을 입력하세요..."
+            onChange={handleInput}
+          />
         </StyledComment>
-        <PostBtn>게시</PostBtn>
-      </CommentWrap>
+        <PostBtn active={inputComment.trim() !== ''} type="submit">
+          게시
+        </PostBtn>
+      </CommentContainer>
     </>
   );
 };
 
 export default Comment;
 
-const CommentWrap = styled.div`
-/* 하단 메뉴처럼 (footer) */
+const CommentContainer = styled.form`
+  padding: 0 1.6rem;
   position: fixed;
   bottom: 0;
-  width: 100%;
-
   display: flex;
   justify-content: space-between;
   height: 6.1rem;
@@ -33,18 +55,20 @@ const CommentWrap = styled.div`
 `;
 
 const StyledComment = styled.div`
-
+  display: flex;
+  align-items: center;
   img {
     width: 3.6rem;
     height: 3.6rem;
-    margin: 1.3rem 0 1.2rem 1.6rem;
+    margin-right: 1.2rem;
   }
 `;
 
 const CommentInput = styled.input`
   border: none;
-  padding: 2.2rem 2rem 1.8rem;
-  width: 60rem;
+  outline: none;
+  width: 100%;
+  padding-right: 11.7rem;
   &::placeholder {
     color: ${({ theme }) => theme.colors.placeHolderColor};
   }
@@ -52,5 +76,19 @@ const CommentInput = styled.input`
 
 const PostBtn = styled.button`
   color: ${({ theme }) => theme.colors.placeHolderColor};
-  margin-right: 2rem;
+  ${({ theme }) =>
+    theme.colors.mainColor &&
+    `
+    &:not([disabled]) {
+      color: ${theme.colors.mainColor};
+      font-weight: 500;
+    }
+  `}
+
+  ${({ active }) =>
+    !active &&
+    `
+    pointer-events: none;
+    opacity: 0.5;
+  `}
 `;
