e43435f14fbc214c695406c2d0e277f69beb96ea
holaleche <whoisleche@gmail.com>
2023-06-23 09:31:04 +0900

    🎨 Style: SNS LoginPage 스타일 수정 (#32)

--- src/pages/SnsLoginPage/SnsLoginPage.jsx
+++ src/pages/SnsLoginPage/SnsLoginPage.jsx
@@ -9,111 +9,121 @@ const SnsLoginPage = () => {
   return (
     <SnsLoginContainer>
       <h1 className='a11y-hidden'>Sns 로그인 페이지</h1>
+      <ContentWrapper>
         <ImgContainer>
-        <SnsMainLogo src={SnsLogo} alt='들숨날숨 로고'/>
-        <MainTitle src={WhiteTitle} alt="들숨날숨"/>
-      </ImgContainer>
-      <SnsLoginMain>
-        <button className='kakao-login'>
-          <GlovalSprite id='message-circle'/>
+          <SnsMainLogo src={SnsLogo} alt='들숨날숨 로고' />
+          <MainTitle src={WhiteTitle} alt="들숨날숨" />
+        </ImgContainer>
+        <SnsLoginMain>
+          <button className='kakao-login'>
+            <GlovalSprite id='message-circle' />
             카카오톡 계정으로 로그인
-        </button>
-        <button className='google-login'>
-          <GlovalSprite id='google-g-logo-1'/>
-            구글 계정으로 로그인</button>
-        <button className='facebook-login'>
-          <GlovalSprite id='facebook'/>
-            페이스북 계정으로 로그인</button>
-        <SnsLoginLink>
-        <Link to='/login' className='login-link'>이메일로 로그인</Link>
-        <Link to='/signup'>회원가입</Link>
-        </SnsLoginLink>
-      </SnsLoginMain>
+          </button>
+          <button className='google-login'>
+            <GlovalSprite id='google-g-logo-1' />
+            구글 계정으로 로그인
+          </button>
+          <button className='facebook-login'>
+            <GlovalSprite id='facebook' />
+            페이스북 계정으로 로그인
+          </button>
+          <SnsLoginLink>
+            <Link to='/login' className='login-link'>이메일로 로그인</Link>
+            <Link to='/signup'>회원가입</Link>
+          </SnsLoginLink>
+        </SnsLoginMain>
+      </ContentWrapper>
     </SnsLoginContainer>
   )
 }
 
-export default SnsLoginPage;
-
 const SnsLoginContainer = styled.main`
-  background-color: ${({theme}) => theme.colors.mainColor};
-  width: 100%;
-  height: 100vh;
+  background-color: ${({ theme }) => theme.colors.mainColor};
+  width: 39rem;
+  height: 70rem;
   display: flex;
   justify-content: center;
-
 `
+
+const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 10%;
+`
+
 const ImgContainer = styled.div`
   display: flex;
   flex-direction: column;
-  margin: 13rem;
+  align-items: center;
+  margin-top: 20rem;
+  margin-bottom: 15rem;
 `
 
 const SnsMainLogo = styled.img`
-  width: 20rem;
-  height: 20rem;
-  margin-bottom: 3rem;
+  width: 100%;
 `
 
 const MainTitle = styled.img`
-width: 17rem;
-height: 6rem;
-margin-left: 1.8rem;
+  margin-top: 1.8rem;
 `
 
 const SnsLoginMain = styled.section`
-  width: 100%;
-  height: 42%;
-  position: absolute;
-  bottom: 0;
-  border-radius: 2rem 2rem 0 0;
+  width: 39rem;
+  height: 30rem;
+  border-radius: 2rem;
   padding: 5rem 3.4rem;
-  background: ${({theme}) => theme.colors.whiteText};
+  background: ${({ theme }) => theme.colors.whiteText};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
   button {
     display: flex;
     justify-content: center;
     align-items: center;
     padding: 1rem;
     border-radius: 4.4rem;
-    width: 100%;
+    width: 32rem;
     height: 4.4rem;
-    font-size: ${({theme}) => theme.fontSize.medium};
-    color: ${({theme}) => theme.colors.textColor};
+    font-size: ${({ theme }) => theme.fontSize.medium};
+    color: ${({ theme }) => theme.colors.textColor};
     margin-bottom: 1rem;
-    justify-content: flex-start; /* 추가 */
-    svg{
+    justify-content: flex-start;
+    svg {
       margin-left: 1rem;
       margin-right: 5rem;
     }
   }
 
-  button:nth-child(2){
-    svg{
+  button:nth-child(2) {
+    svg {
       margin-right: 6.3rem;
     }
   }
 
   .kakao-login {
-    border: 0.1rem solid ${({theme}) => theme.colors.kakaoColor};
+    border: 0.1rem solid ${({ theme }) => theme.colors.kakaoColor};
   }
   .google-login {
-    border: 0.1rem solid ${({theme}) => theme.colors.googleColor}
+    border: 0.1rem solid ${({ theme }) => theme.colors.googleColor};
   }
   .facebook-login {
-    border: 0.1rem solid ${({theme}) => theme.colors.facebookColor};
+    border: 0.1rem solid ${({ theme }) => theme.colors.facebookColor};
     margin-bottom: 2rem;
   }
 `
 
 const SnsLoginLink = styled.div`
-text-align: center;
-color: ${({theme}) => theme.colors.textColor};
-font-size: ${({theme}) => theme.fontSize.small};
-.login-link::after {
-  content: '|';
-  padding: 1rem;
-  color: ${({theme}) => theme.colors.placeHolderColor};
-}
+  text-align: center;
+  color: ${({ theme }) => theme.colors.textColor};
+  font-size: ${({ theme }) => theme.fontSize.small};
+  .login-link::after {
+    content: '|';
+    padding: 1rem;
+    color: ${({ theme }) => theme.colors.placeHolderColor};
+  }
 `
 
-
+export default SnsLoginPage;
