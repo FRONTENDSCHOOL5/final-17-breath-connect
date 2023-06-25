c107b0b5e2c4c881f5941e46a91dde62100116d5
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2023-06-26 02:55:37 +0900

    ✨ Feat: 모달 로그아웃 기능 구현

--- src/components/common/Alert/LogOut.jsx
+++ src/components/common/Alert/LogOut.jsx
@@ -1,8 +1,23 @@
 import React, {useState} from 'react';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import { useSetRecoilState, useRecoilCallback } from 'recoil';
+import { loginAtom } from '../../../atoms/LoginAtom';
+import { tokenAtom, accountAtom, profileImgAtom, usernameAtom, introAtom } from '../../../atoms/UserAtom';
 
 const LogOut = ({ message, onClose, done }) => {
+  const navigate = useNavigate();
   const [showLogOut, setShowLogOut] = useState(true);
+  const setLoginState = useSetRecoilState(loginAtom);
+
+  /* reset 메서드를 사용하여 atom 값 초기화 */
+   const handleResetState = useRecoilCallback(({ reset }) => () => {
+    reset(tokenAtom);
+    reset(accountAtom);
+    reset(profileImgAtom);
+    reset(usernameAtom);
+    reset(introAtom);
+  });
 
   const handleClickCancel = () => {
     setShowLogOut(false);
@@ -13,6 +28,13 @@ const LogOut = ({ message, onClose, done }) => {
     setShowLogOut(false);
     onClose(true); // 삭제 완료를 알리기 위해 onClose(true) 호출
   };
+  const handleLogout = () => {
+    setLoginState(false);
+    handleResetState();
+    localStorage.removeItem('token');
+    navigate('/');
+  };
+
 
   return (
     <LogOutAlert>
@@ -20,7 +42,10 @@ const LogOut = ({ message, onClose, done }) => {
         <ButtonText>{message}</ButtonText>
         <ButtonContainer>
           <CancelButton onClick={handleClickCancel}>취소</CancelButton>
-          <LogOutButton onClick={handleClick}>로그아웃</LogOutButton>
+          <LogOutButton onClick={() => {
+            handleClick();
+            handleLogout();
+          }}>로그아웃</LogOutButton>
         </ButtonContainer>
       </AlertContainer>
     </LogOutAlert>
