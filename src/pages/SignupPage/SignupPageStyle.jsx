869ab786c2482c56ac7cae2f3784210c3e2aa63a
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2023-07-13 00:14:14 +0900

    🎨 Style: 회원가입 페이지 스타일 컴포넌트 분리 (#204)

---
+++ src/pages/SignupPage/SignupPageStyle.jsx
@@ -0,0 +1,33 @@
+import styled, { css } from 'styled-components';
+
+export const Container = styled.main`
+  margin: 0 auto;
+`
+
+export const Title = styled.h1`
+padding-top: 2.7rem;
+  color: ${({ theme }) => theme.colors.blackText};
+  font-size: ${({ theme }) => theme.fontSize.xxlarge};
+  text-align: center;
+  margin-bottom: 4.5rem;
+`
+
+export const Form = styled.form`
+   .input-wrapper {
+    margin-bottom: 3rem; 
+  } 
+`
+
+export const ErrorMsg = styled.p`
+  ${({ theme }) => css`
+    color: ${theme.colors.errorText};
+    font-size: ${theme.fontSize.small};
+    margin-top: -0.9rem;
+  `}
+  &.password-msg {
+    margin: -2.4rem 0 3rem;
+  }
+  &.success-msg {
+    color: blue;
+  }
+`;
