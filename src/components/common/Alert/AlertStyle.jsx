61398d9b1f5f46c71c016ef53cfba975a54c7d9f
holaleche <whoisleche@gmail.com>
2023-07-17 10:15:22 +0900

    🎨 Style: Alert 스타일 파일 정리 (#204)

---
+++ src/components/common/Alert/AlertStyle.jsx
@@ -0,0 +1,50 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 999;
+  opacity: 1;
+  pointer-events: auto;
+`
+
+export const AlertContainer = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  flex-direction: column;
+  width: 25.2rem;
+  background-color: ${({ theme }) => theme.colors.whiteText};
+  border-radius: 1rem;
+  border: 0.1rem solid ${({ theme }) => theme.colors.borderColor};
+`;
+
+export const AlertMessage = styled.p`
+  text-align: center;
+  padding: 2rem 0rem;
+  font-size: ${({ theme }) => theme.fontSize.large};
+`
+
+export const ButtonContainer = styled.div`
+border-top: 0.05rem solid ${({ theme }) => theme.colors.borderColor};
+font-size: ${({ theme }) => theme.fontSize.medium};
+  button {
+    width: 12.5rem;
+    padding: 1.3rem 0;
+  }
+`;
+export const CancelButton = styled.button`
+`
+
+export const DeleteButton = styled.button`
+  border-left: 0.05rem solid ${({ theme }) => theme.colors.borderColor};
+  color: ${({ theme }) => theme.colors.mainColor};
+`;
