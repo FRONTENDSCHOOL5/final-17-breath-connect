d4c9fe2821ba0b016528898fd41d91bb7f62ffc6
holaleche <whoisleche@gmail.com>
2023-08-05 18:46:58 +0900

    ♻️ Refactor: 다크모드 코드 정리 2차 (#231)

--- src/components/Header/BackButton.jsx
+++ src/components/Header/BackButton.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
-import { iconColorSelector, isDarkModeState } from '../../atoms/StylesAtom';
+import { isDarkModeState } from '../../atoms/StylesAtom';
 import GlobalSprite from '../../assets/sprite/GlobalSprite';
 import { Button } from './BackButtonStyle'
 
 const BackButton = () => {
   const navigate = useNavigate();
-  const iconColor = useRecoilValue(iconColorSelector);
   const isDarkMode = useRecoilValue(isDarkModeState);
   const goBack = () => {
     navigate(-1);
@@ -15,7 +14,7 @@ const BackButton = () => {
 
   return (
     <Button onClick={goBack}>
-      <GlobalSprite id={isDarkMode ? 'icon-arrow-left-dark' : 'icon-arrow-left'} color={iconColor} />
+      <GlobalSprite id={isDarkMode ? 'icon-arrow-left-dark' : 'icon-arrow-left'} color={'transparent'}/>
     </Button>
   );
 };
