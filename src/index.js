4b2aaae9136f999df2ddf2f6d69038366cf97c99
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2023-10-19 17:22:44 +0900

    🔥 Remove: 사용되지 않는 변수 및 파일 삭제

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import ReactDOM from 'react-dom'; //구버전
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { RecoilRoot } from 'recoil';
--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -1,5 +1,4 @@
 import { createGlobalStyle } from 'styled-components';
-import font from './font.css';
 import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
