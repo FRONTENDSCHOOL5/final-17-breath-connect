74a91c3a0dd77c1cd5f993e9ebe0543fafb99ddc
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2023-10-24 21:54:31 +0900

    🚚 Remove: recoilRoot app.js로 이동

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import { RecoilRoot } from 'recoil';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
-  <RecoilRoot>
     <App />
-  </RecoilRoot>
 );
