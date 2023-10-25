2e1267aa017b5b628db719b43fb7b798b0c2b7d9
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2023-10-25 23:57:00 +0900

    📦 Chore: react-query 라이브러리 세팅

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import GlobalStyle from './styles/GlobalStyle';
 import { RecoilRoot } from 'recoil';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { ThemeProvider } from 'styled-components';
 import Theme from './styles/Theme';
 import BasicLayout from './styles/BasicLayout';
