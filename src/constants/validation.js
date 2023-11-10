b6d67ab0311310eaf5a6cc8a378af949cadc070c
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2023-11-10 23:24:23 +0900

    ✨ Feat: 사용자 이름 및 계정 ID 상수 추가

--- src/constants/validation.js
+++ src/constants/validation.js
@@ -1,6 +1,8 @@
 export const PATTERN = {
   EMAIL: /^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zAZ0-9-.]+$/,
   PASSWORD: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{6,}$/,
+  USERNAME: /^[a-zA-Z0-9]{2,10}$/,
+  ACCOUNTNAME: /^[a-zA-Z0-9._]+$/
 };
 
 export const MESSAGE = {
@@ -10,6 +12,14 @@ export const MESSAGE = {
   },
   PASSWORD: {
     REQUIRED: '비밀번호를 입력해주세요',
-    PATTERN: '영문+숫자+특수기호 조합으로 6자리 이상 입력해주세요'
+    PATTERN: '영문 + 숫자 + 특수기호 조합으로 6자리 이상 입력해주세요'
+  },
+  USERNAME: {
+    REQUIRED: '사용자 이름을 입력해주세요',
+    PATTERN: '영문 2 ~ 10자 이내로 입력해주세요'
+  },
+  ACCOUNTNAME: {
+    REQUIRED: '계정 ID를 입력해주세요',
+    PATTERN: '영문, 숫자, 특수문자 ., _ 만 입력해주세요'
   }
 }
