af3f1eae91b1209a40703a910a6ac6db58c4f538
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2023-11-27 23:00:16 +0900

    🎨 Style: 로딩 컴포넌트 추가

--- src/components/common/Loading/Loading.jsx
+++ src/components/common/Loading/Loading.jsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import {
-  GlobalStyle,
-  Container,
-  Body,
-  Circle,
-  Before,
-  Text,
-} from './LoadingStyle';
+import styled from 'styled-components';
+import { ReactComponent as Spinner} from '../../../assets/images/spinner.svg';
 
 const Loading = () => {
   return (
-    <Container>
-      <GlobalStyle />
-      <Body>
-          <Circle>
-            <Before />
-          </Circle>
-          <Text>Loading...</Text>
-      </Body>
-    </Container>
+    <>
+      <Container>
+        <Spinner />
+      </Container>
+    </>
   );
 };
 
 export default Loading;
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
